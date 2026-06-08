a84e1440c4d1fcd4a5d5757b6cf552c540b34f36
GTXtab <malahovskinazar007@gmail.com>
2026-06-08 15:54:59 +0300

    fix: resolved bugs

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
 import usersReducer from '../features/usersSlice';
+import postsReducer from '../features/postsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     users: usersReducer,
+    posts: postsReducer,
   },
 });
 
---
+++ src/features/postsSlice.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { getUserPosts } from '../api/posts';
+import { Post } from '../types/Post';
+
+export interface PostsState {
+  items: Post[];
+  loaded: boolean;
+  hasError: boolean;
+  selectedPost: Post | null;
+}
+
+const initialState: PostsState = {
+  items: [],
+  loaded: false,
+  hasError: false,
+  selectedPost: null,
+};
+
+export const fetchPosts = createAsyncThunk(
+  'posts/fetch',
+  async (userId: number) => {
+    return getUserPosts(userId);
+  },
+);
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    setSelectedPost: (state, action: PayloadAction<Post | null>) => {
+      state.selectedPost = action.payload;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchPosts.pending, state => {
+        state.loaded = false;
+        state.hasError = false;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action: PayloadAction<Post[]>) => {
+        state.items = action.payload;
+        state.loaded = true;
+      })
+      .addCase(fetchPosts.rejected, state => {
+        state.loaded = true;
+        state.hasError = true;
+      });
+  },
+});
+
+export const { setSelectedPost } = postsSlice.actions;
+export default postsSlice.reducer;
